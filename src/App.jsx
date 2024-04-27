d31a1f4ad9a188e1d5d50e1fc31e0e45f0391874
Adeyemi Oyedeji <89479407+AnosVoldigoad999@users.noreply.github.com>
2024-04-27 02:48:48 +0100

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,137 @@ const [jobs, setJobs] = useState(()=>{
  return jobS
 });
 
-const testJob = jobs[0]
+
+const [filters, setFilters] = useState([])
+const [isFiltering, setIsFiltering] = useState(false)
+const [filteredList, setFilteredList] = useState([])
+/*functions*/
+function handleFilter(filter){
+
+  setIsFiltering(true)
+  let newFilteredList = []
+  /*change the filtered list broski!*/
+  let langExists = false
+  /*jobs.forEach(job=>{
+    let langs = job.languages
+    for(let i=0; i<langs.length; i++){
+      if(filter===langs[i]){
+        newFilteredList.push(job)
+      }
+    }
+  })*/
+  
 
 
-console.log(testJob)
+  jobs.forEach(job=>{
+    //check languages
+    let langs = job.languages
+ 
+
+    for(let i=0; i<(filters.length!=0?filters.length:1); i++){
+      if(filters.length===0){
+        if(job.role===filter || job.level===filter){
+        
+          newFilteredList.push(job)
+          //console.log(job)
+          console.log("i ran")
+        }
+      }else{
+        if(job.role===filters[i] || job.level===filters[i]){
+            //if the job isnt in the list already, add it
+          for(let j=0; j<newFilteredList.length; j++){
+            if(newFilteredList[j]!=job){
+              newFilteredList.push(job)
+            }
+          }
+          console.log("i also ran")
+          
+          //console.log(job)
+        }
+      }
+    }
+
+    for(let i=0; i<langs.length; i++){
+      if(filter===langs[i]){
+        newFilteredList.push(job)
+      }
+    }
+
+    /*change the filtered list broski!*/
+    /*for(let i=0; i<(filters.length!=0?filters.length:3); i++){
+    
+    }*/
+  })
+  setFilteredList(newFilteredList)
+  /*console.log(newFilteredList)*/
+  let isIn = false
+  //if its in dont run lmao*/
+  if(filters.length<8 && filters.length!=0){
+    for(let i=0; i<filters.length; i++){
+      if(filters[i]===filter){
+        isIn = true
+        console.log("its in, genius...smh", isIn)
+      }
+    }
+
+    if(!isIn){
+      setFilters([...filters, filter])
+    }
+
+  }else if (filters.length!=8){
+    setFilters([...filters, filter])
+  }
+  
+  
+
+}
+
+
+function handleDelete(index){
+  setFilters(filters.filter(filter=>filter!=filters[index]))
+  if(filters.length===1){
+    setIsFiltering(false)
+  }
+  //console.log(index)
+}
+
+function handleClear(){
+  setFilters([])
+  setIsFiltering(false)
+}
+
+
+//functions
+
+
+
+//const testJob = jobs[0]
+
+
+//console.log(testJob)
   return <>
   <img src="/images/bg-header-desktop.svg" alt="" className='headerBg' />
-
+ 
   <main>
+  {filters.length > 0 &&  <div className="filterCont">
+    <div className="filters">
+   {filters.map((filter, index)=>{
+    return  <div className="filter" >
+    <span className="left">
+      {filter}
+    </span>
+    <img src="public\images\icon-remove.svg" alt="x" className="right" onClick={()=>{handleDelete(index)}} />
+  </div>
+   })}
+    </div>
+    <span className="clear" onClick={handleClear}>
+      Clear
+    </span>
+  </div> }
     <div className="jobs">
       {/*JOBS*/}
 
-      {jobs.map(job=>{
+      {(isFiltering?filteredList:jobs).map(job=>{
             return <div className="job">
              {/*profile*/}
              <img src={job.logo} alt="prof" className='prof' />
@@ -49,14 +168,14 @@ console.log(testJob)
      
              <div className="end">
                <div className="cats">
-                 <div className="cat">
+                 <div className="cat" onClick={()=>handleFilter(job.role)}>
                    {job.role}
                  </div>
-                 <div className="cat">
+                 <div className="cat" onClick={()=>handleFilter(job.level)}>
                    {job.level}
                  </div>
                  {job.languages.map(lang=>{
-                   return <div className="cat">
+                   return <div className="cat" onClick={()=>handleFilter(lang)}>
                      {lang}
                    </div>
                  })}
